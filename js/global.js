bf493fe48e704f795f33b1d9c80c2f1a8001ccec
Johnny Taylor <johnny@abledaccess.com>
2015-02-17 12:51:58 -0500

    Add basic Grunt functionality
    To watch/ build and minify both 'js' and 'css' assets. Plus fix
    a broken Modernizr. And add one more file, '.sass-cache', to
    '.gitignore'.

---
+++ js/global.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+	setTimeout(function() {
+		// preload wallpaper
+		new Image().src = "http://mattmacintosh.com/images/wallpaper/Real-Estates_WP.jpg";
+		new Image().src = "http://mattmacintosh.com/images/wallpaper/Subject_WP.jpg";
+		new Image().src = "http://mattmacintosh.com/images/wallpaper/Mac-OS-CMYK_Davos.WP.jpg";
+		new Image().src = "http://mattmacintosh.com/images/wallpaper/Nagarjuna-and-Difference_WP.jpg";
+		new Image().src = "http://mattmacintosh.com/images/wallpaper/Dog-Caterpillar_WP.jpg";
+		new Image().src = "http://mattmacintosh.com/images/wallpaper/Illuminated-Manuscripts_WP.jpg";
+	}, 1000);
+};
