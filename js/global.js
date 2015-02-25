982b4239b7a718d3856667ed5cee11529e9fa01d
Johnny Taylor <johnny@abledaccess.com>
2015-02-25 17:14:10 -0500

    MASTER: Add image attributes to backstretch images

--- js/global.js
+++ js/global.js
@@ -9,3 +9,46 @@ window.onload = function() {
 		new Image().src = "http://mattmacintosh.com/images/wallpaper/Illuminated-Manuscripts_WP.jpg";
 	}, 1000);
 };
+
+function realEstates() {
+	var backStretchImage = $.backstretch(["http://mattmacintosh.com/images/wallpaper/Real-Estates_WP.jpg"]);
+	var backStretchImageAlt = $(".backstretch img").attr("alt", "Image description coming soon");
+	var backStretchImageHeight = $(".backstretch img").attr("height", "264");
+	var backStretchImageWidth = $(".backstretch img").attr("width", "400");
+	var backStretchImageARIAHidden = $(".backstretch img").attr("aria-hidden", "true");
+}
+function subject() {
+	var backStretchImage = $.backstretch(["http://mattmacintosh.com/images/wallpaper/Subject_WP.jpg"]);
+	var backStretchImageAlt = $(".backstretch img").attr("alt", "Image description coming soon");
+	var backStretchImageHeight = $(".backstretch img").attr("height", "264");
+	var backStretchImageWidth = $(".backstretch img").attr("width", "400");
+	var backStretchImageARIAHidden = $(".backstretch img").attr("aria-hidden", "true");
+}
+function cmyk() {
+	var backStretchImage = $.backstretch(["http://mattmacintosh.com/images/wallpaper/Mac-OS-CMYK_Davos.WP.jpg"]);
+	var backStretchImageAlt = $(".backstretch img").attr("alt", "Image description coming soon");
+	var backStretchImageHeight = $(".backstretch img").attr("height", "264");
+	var backStretchImageWidth = $(".backstretch img").attr("width", "400");
+	var backStretchImageARIAHidden = $(".backstretch img").attr("aria-hidden", "true");
+}
+function nagarjuna() {
+	var backStretchImage = $.backstretch(["http://mattmacintosh.com/images/wallpaper/Nagarjuna-and-Difference_WP.jpg"]);
+	var backStretchImageAlt = $(".backstretch img").attr("alt", "Image description coming soon");
+	var backStretchImageHeight = $(".backstretch img").attr("height", "264");
+	var backStretchImageWidth = $(".backstretch img").attr("width", "400");
+	var backStretchImageARIAHidden = $(".backstretch img").attr("aria-hidden", "true");
+}
+function dog() {
+	var backStretchImage = $.backstretch(["http://mattmacintosh.com/images/wallpaper/Dog-CaterpillarSKY_WP.jpg"]);
+	var backStretchImageAlt = $(".backstretch img").attr("alt", "Image description coming soon");
+	var backStretchImageHeight = $(".backstretch img").attr("height", "264");
+	var backStretchImageWidth = $(".backstretch img").attr("width", "400");
+	var backStretchImageARIAHidden = $(".backstretch img").attr("aria-hidden", "true");
+}
+function illuminated() {
+	var backStretchImage = $.backstretch(["http://mattmacintosh.com/images/wallpaper/Illuminated-Manuscripts_WP.jpg"]);
+	var backStretchImageAlt = $(".backstretch img").attr("alt", "Image description coming soon");
+	var backStretchImageHeight = $(".backstretch img").attr("height", "264");
+	var backStretchImageWidth = $(".backstretch img").attr("width", "400");
+	var backStretchImageARIAHidden = $(".backstretch img").attr("aria-hidden", "true");
+}
