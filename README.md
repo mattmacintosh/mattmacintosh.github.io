3070da32d0fff7d83aec7b5e4c3b5f390d10fd94
Johnny Taylor <johnny@abledaccess.com>
2023-04-09 16:04:51 -0400

    Change http protocol references to https

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 
 "Matt Macintosh is a Kamloops–based artist working with found images and objects, painting, video and sound. His work explores the effects of erasure, systematization and repetition on cultural canon materials as they relate to fundamental human experiences like the longing for emancipation. Currently Curator at the Kamloops Museum and Archives, he has exhibited in Canada and the US." 
 
-And this repository is hosted on Github Pages and <a href="http://mattmacintosh.com">the resulting website</a> is generated with Jekyll.
+And this repository is hosted on Github Pages and <a href="https://mattmacintosh.com">the resulting website</a> is generated with Jekyll.
--- js/global.js
+++ js/global.js
@@ -1,13 +1,13 @@
 window.onload = function() {
 	setTimeout(function() {
 		// preload wallpaper
-		new Image().src = "http://mattmacintosh.com/images/wallpaper/Penicillin_WP.jpg";
-		new Image().src = "http://mattmacintosh.com/images/wallpaper/Real-Estates_WP.jpg";
-		new Image().src = "http://mattmacintosh.com/images/wallpaper/Subject_WP.jpg";
-		new Image().src = "http://mattmacintosh.com/images/wallpaper/Mac-OS-CMYK_Davos.WP.jpg";
-		new Image().src = "http://mattmacintosh.com/images/wallpaper/Nagarjuna-and-Difference_WP.jpg";
-		new Image().src = "http://mattmacintosh.com/images/wallpaper/Dog-CaterpillarSKY_WP.jpg";
-		new Image().src = "http://mattmacintosh.com/images/wallpaper/Illuminated-Manuscripts_WP.jpg";
+		new Image().src = "https://mattmacintosh.com/images/wallpaper/Penicillin_WP.jpg";
+		new Image().src = "https://mattmacintosh.com/images/wallpaper/Real-Estates_WP.jpg";
+		new Image().src = "https://mattmacintosh.com/images/wallpaper/Subject_WP.jpg";
+		new Image().src = "https://mattmacintosh.com/images/wallpaper/Mac-OS-CMYK_Davos.WP.jpg";
+		new Image().src = "https://mattmacintosh.com/images/wallpaper/Nagarjuna-and-Difference_WP.jpg";
+		new Image().src = "https://mattmacintosh.com/images/wallpaper/Dog-CaterpillarSKY_WP.jpg";
+		new Image().src = "https://mattmacintosh.com/images/wallpaper/Illuminated-Manuscripts_WP.jpg";
 	}, 1000);
 };
 
@@ -18,49 +18,49 @@ function homePage() {
 	var backStretchImageARIAHidden = $(".backstretch img").attr("aria-hidden", "true");
 }
 function paintings() {
-	var backStretchImage = $.backstretch(["http://mattmacintosh.com/images/wallpaper/Paintings_WP.jpg"]);
+	var backStretchImage = $.backstretch(["https://mattmacintosh.com/images/wallpaper/Paintings_WP.jpg"]);
 	var backStretchImageHeight = $(".backstretch img").attr("height", "1");
 	var backStretchImageWidth = $(".backstretch img").attr("width", "1");
 	var backStretchImageARIAHidden = $(".backstretch img").attr("aria-hidden", "true");
 }
 function typetoken() {
-	var backStretchImage = $.backstretch(["http://mattmacintosh.com/images/wallpaper/Map.1812.Wallpaper.jpg"]);
+	var backStretchImage = $.backstretch(["https://mattmacintosh.com/images/wallpaper/Map.1812.Wallpaper.jpg"]);
 	var backStretchImageHeight = $(".backstretch img").attr("height", "1");
 	var backStretchImageWidth = $(".backstretch img").attr("width", "1");
 	var backStretchImageARIAHidden = $(".backstretch img").attr("aria-hidden", "true");
 }
 function realEstates() {
-	var backStretchImage = $.backstretch(["http://mattmacintosh.com/images/wallpaper/Real-Estates_WP.jpg"]);
+	var backStretchImage = $.backstretch(["https://mattmacintosh.com/images/wallpaper/Real-Estates_WP.jpg"]);
 	var backStretchImageHeight = $(".backstretch img").attr("height", "1");
 	var backStretchImageWidth = $(".backstretch img").attr("width", "1");
 	var backStretchImageARIAHidden = $(".backstretch img").attr("aria-hidden", "true");
 }
 function subject() {
-	var backStretchImage = $.backstretch(["http://mattmacintosh.com/images/wallpaper/Subject_WP.jpg"]);
+	var backStretchImage = $.backstretch(["https://mattmacintosh.com/images/wallpaper/Subject_WP.jpg"]);
 	var backStretchImageHeight = $(".backstretch img").attr("height", "2");
 	var backStretchImageWidth = $(".backstretch img").attr("width", "3");
 	var backStretchImageARIAHidden = $(".backstretch img").attr("aria-hidden", "true");
 }
 // function cmyk() {
-// 	var backStretchImage = $.backstretch(["http://mattmacintosh.com/images/wallpaper/Mac-OS-CMYK_Davos.WP.jpg"]);
+// 	var backStretchImage = $.backstretch(["https://mattmacintosh.com/images/wallpaper/Mac-OS-CMYK_Davos.WP.jpg"]);
 // 	var backStretchImageHeight = $(".backstretch img").attr("height", "1");
 // 	var backStretchImageWidth = $(".backstretch img").attr("width", "1");
 // 	var backStretchImageARIAHidden = $(".backstretch img").attr("aria-hidden", "true");
 // }
 function nagarjuna() {
-	var backStretchImage = $.backstretch(["http://mattmacintosh.com/images/wallpaper/Nagarjuna-and-Difference_WP.jpg"]);
+	var backStretchImage = $.backstretch(["https://mattmacintosh.com/images/wallpaper/Nagarjuna-and-Difference_WP.jpg"]);
 	var backStretchImageHeight = $(".backstretch img").attr("height", "1");
 	var backStretchImageWidth = $(".backstretch img").attr("width", "1");
 	var backStretchImageARIAHidden = $(".backstretch img").attr("aria-hidden", "true");
 }
 function dog() {
-	var backStretchImage = $.backstretch(["http://mattmacintosh.com/images/wallpaper/Dog-CaterpillarSKY_WP.jpg"]);
+	var backStretchImage = $.backstretch(["https://mattmacintosh.com/images/wallpaper/Dog-CaterpillarSKY_WP.jpg"]);
 	var backStretchImageHeight = $(".backstretch img").attr("height", "1");
 	var backStretchImageWidth = $(".backstretch img").attr("width", "1");
 	var backStretchImageARIAHidden = $(".backstretch img").attr("aria-hidden", "true");
 }
 function illuminated() {
-	var backStretchImage = $.backstretch(["http://mattmacintosh.com/images/wallpaper/IM_Pattern_WP.jpg"]);
+	var backStretchImage = $.backstretch(["https://mattmacintosh.com/images/wallpaper/IM_Pattern_WP.jpg"]);
 	var backStretchImageHeight = $(".backstretch img").attr("height", "1");
 	var backStretchImageWidth = $(".backstretch img").attr("width", "1");
 	var backStretchImageARIAHidden = $(".backstretch img").attr("aria-hidden", "true");
